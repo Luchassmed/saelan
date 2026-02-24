0bb4735a005f87c0a7c6994a634ca884f3729bd5
Luchassmed <kapot@live.dk>
2026-02-24 14:55:37 +0100

    contact details update

--- app/contact/page.js
+++ app/contact/page.js
@@ -3,32 +3,18 @@ export default function ContactPage() {
     <main className="fade-in min-h-[calc(100vh-4rem)] flex items-center justify-center px-6">
       <div className="text-center text-sm tracking-widest space-y-6 max-w-xl">
         <p>
-          DET ER IKKE MULIGT AT KONTAKTE FORFATTEREN AF DENNE SIDE.
+          Oskar Sælan Halskov
           <br />
-          <em className="italic">
-            IT IS NOT POSSIBLE TO CONTACT THE AUTHOR OF THIS SITE.
-          </em>
+          oskar@saelan.dk
+          <br />
+          0045 50359085
         </p>
 
-        <div className="text-xs leading-relaxed tracking-normal text-left md:text-center">
+        <div className="text-left md:text-center">
           <p className="mb-2">
-            HVIS DU VIL SE &quot;TEGNINGERNE&quot; BAG SIDEN, LIGGER KILDEKODEN
-            OFFENTLIGT TILGÆNGELIG HER.
+            Hjemmeside/Webpage
             <br />
-            <em className="italic">
-              IF YOU WANT TO SEE THE &quot;DRAWINGS&quot; OF THE SITE, THE
-              SOURCE CODE IS PUBLICLY AVAILABLE HERE.
-            </em>
-          </p>
-          <p>
-            <a
-              href="https://github.com/Luchassmed/saelan"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="underline underline-offset-2"
-            >
-              GITHUB.COM/LUCHASSMED/SAELAN
-            </a>
+            <p>Luchas Schmidt</p>
           </p>
         </div>
       </div>
